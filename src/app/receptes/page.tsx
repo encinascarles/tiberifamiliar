561d9900a0e4862dd645657562d326c4a1974cc5
Carles Encinas <carles@ganxo.net>
2024-04-13 18:42:08 +0200

    Created public recipes screen

--- src/app/receptes/page.tsx
+++ src/app/receptes/page.tsx
@@ -1,3 +1,16 @@
+import RecipeCard from "@/components/RecipeCard";
+
 export default function PublicRecipesPage() {
-  return <h1>PublicRecipesPage</h1>;
+  return (
+    <div className="container">
+      <div className="flex justify-start items-center gap-6">
+        <h1 className="text-4xl font-bold my-10 mr-10">Receptes Publiques</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <RecipeCard key={i} />
+        ))}
+      </div>
+    </div>
+  );
 }
