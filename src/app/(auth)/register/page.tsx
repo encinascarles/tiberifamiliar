cc0f1fba891a95b79e3c2da56f93e44e6ec1ee28
Carles Encinas <carles@ganxo.net>
2024-04-14 23:17:15 +0200

    fixed ' error

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -10,7 +10,9 @@ export default function RegisterPage() {
         Entra el teu correu electrònic per continuar
       </p>
       <Input placeholder="Correu Electrònic"></Input>
-      <Button className="w-full">Registra't amb correu electrònic</Button>
+      <Button className="w-full">
+        Registra{"&apos"}t amb correu electrònic
+      </Button>
       <div className="relative w-full">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t"></span>
