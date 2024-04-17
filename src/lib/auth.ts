2f561e1ee9930fd7068e6d509dd6f59cd7030e27
Carles Encinas <carles@ganxo.net>
2024-04-17 17:53:46 +0200

    Practice on callbacks (all commented except for adding id to session)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "@/db/db";
+import { getUserById } from "@/db/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,38 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id as string);
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+    session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      // if (token.username && session.user)
+      //   session.user.username = token.username;
+      // }
+
+      return session;
+    },
+    // async jwt({ token }) {
+    //   if (!token.sub) return token;
+
+    //   console.log("buscant usuari...");
+    //   const existingUser = await getUserById(token.sub);
+
+    //   if (!existingUser) return token;
+
+    //   token.username = existingUser.username;
+
+    //   return token;
+    // },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
