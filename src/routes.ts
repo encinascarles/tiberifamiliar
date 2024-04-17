b4ff0c65b1240611bc95e8d0a428451c227ae668
Carles Encinas <carles@ganxo.net>
2024-04-18 00:13:26 +0200

    Reset password page

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
-export const publicRoutes = [
-  "/",
-  "/perfil",
-  "/new-verification",
+export const publicRoutes = ["/", "/perfil", "/new-verification"];
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/autherror",
   "/reset-password",
   "/new-password",
 ];
-export const authRoutes = ["/login", "/register", "/autherror"];
 export const apiAuthPrefix = "/api/auth";
 export const DEFAULT_LOGIN_REDIRECT = "/";
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -22,10 +22,22 @@ export const RegisterSchema = z
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Las contraseñas deben coincidir.",
+    message: "Les dontrassenyes han de coincidir.",
     path: ["confirmPassword"], // this will point the error to 'confirmPassword' field
   });
 
 export const PasswordResetSchema = z.object({
   email: z.string().email({ message: "El correu electrònic no és vàlid." }),
 });
+
+export const NewPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(5, { message: "La contrassenya ha de tenir 5 o més caràcters" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Les dontrassenyes han de coincidir.",
+    path: ["confirmPassword"], // this will point the error to 'confirmPassword' field
+  });
