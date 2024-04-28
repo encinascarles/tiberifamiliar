efaa215cfcb4f79f6ca230b57b43bc8c7eeb127d
Carles Encinas <carles@ganxo.net>
2024-04-28 19:43:49 +0200

    Created autherror page

--- app/(auth)/autherror/page.tsx
+++ app/(auth)/autherror/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const AuthErrorPage = () => {
   return (
-    <div>
-      <h1>Error autoritzant</h1>
+    <div className=" flex flex-col items-center gap-2 w-full mx-auto sm:w-[350px]">
+      <h1 className="text-2xl font-semibold tracking-tight">
+        Error amb l{"'"}autenticació
+      </h1>
+      <p className="text-sm text-muted-foreground">Torna-ho a intentar</p>
+      <div className="space-x-4 mt-8">
+        <Link href="/login">
+          <Button>Inicia sessió</Button>
+        </Link>
+        <Link href="/register">
+          <Button>Registra{"'"}t</Button>
+        </Link>
+      </div>
     </div>
   );
 };
