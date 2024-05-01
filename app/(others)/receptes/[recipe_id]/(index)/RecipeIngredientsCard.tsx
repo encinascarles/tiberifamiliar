bcd0b9435dcbc403a86d5480c07de24fdd2b1a92
Carles Encinas <carles@ganxo.net>
2024-05-01 19:35:55 +0200

    Ingredients cross when checked

--- app/(others)/receptes/[recipe_id]/(index)/RecipeIngredientsCard.tsx
+++ app/(others)/receptes/[recipe_id]/(index)/RecipeIngredientsCard.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { recipeAndAuthor } from "@/types";
+import { useState } from "react";
 
 interface RecipeIngredientsCardProps {
   recipe: recipeAndAuthor;
@@ -9,6 +11,19 @@ interface RecipeIngredientsCardProps {
 const RecipeIngredientsCard: React.FC<RecipeIngredientsCardProps> = ({
   recipe,
 }) => {
+  const [checkedItems, setCheckedItems] = useState(
+    new Array(recipe.ingredients.length).fill(false)
+  );
+
+  const handleCheckChange = (index: number) => {
+    console.log(index);
+    setCheckedItems((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -18,8 +33,20 @@ const RecipeIngredientsCard: React.FC<RecipeIngredientsCardProps> = ({
         <ul className="list-none">
           {recipe?.ingredients.map((ingredient, i) => (
             <li key={i} className="flex items-center gap-2 text-lg my-3">
-              <Checkbox className="w-6 h-6" />
-              {ingredient}
+              <Checkbox
+                checked={checkedItems[i]}
+                onCheckedChange={() => handleCheckChange(i)}
+                className="w-6 h-6"
+              />
+              <span
+                className={
+                  checkedItems[i]
+                    ? "line-through decoration-orange-600 decoration-2"
+                    : ""
+                }
+              >
+                {ingredient}
+              </span>
             </li>
           ))}
         </ul>
