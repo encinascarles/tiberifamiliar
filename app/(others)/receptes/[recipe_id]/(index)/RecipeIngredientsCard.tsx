35b56bb08eb6a50a9b3101acba3e27e327972459
Carles Encinas <carles@ganxo.net>
2024-05-01 23:45:03 +0200

    prova

--- app/(others)/receptes/[recipe_id]/(index)/RecipeIngredientsCard.tsx
+++ app/(others)/receptes/[recipe_id]/(index)/RecipeIngredientsCard.tsx
@@ -1,7 +1,10 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import { recipeAndAuthor } from "@/types";
+import { Minus, Plus } from "lucide-react";
 import { useState } from "react";
 
 interface RecipeIngredientsCardProps {
@@ -14,6 +17,11 @@ const RecipeIngredientsCard: React.FC<RecipeIngredientsCardProps> = ({
   const [checkedItems, setCheckedItems] = useState(
     new Array(recipe.ingredients.length).fill(false)
   );
+  const [servings, setServings] = useState(recipe.servings.toString());
+
+  const increment = () => setServings((Number(servings) + 1).toString());
+  const decrement = () =>
+    setServings((Number(servings) > 1 ? Number(servings) - 1 : 1).toString());
 
   const handleCheckChange = (index: number) => {
     console.log(index);
@@ -24,10 +32,53 @@ const RecipeIngredientsCard: React.FC<RecipeIngredientsCardProps> = ({
     });
   };
 
+  const handleServingsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      e.target.value === "" ||
+      (!isNaN(Number(e.target.value)) && Number(e.target.value) > 0)
+    ) {
+      setServings(e.target.value);
+      return;
+    }
+  };
+
+  const handleBlur = () => {
+    if (isNaN(Number(servings)) || Number(servings) < 1) {
+      setServings(recipe.servings.toString());
+    }
+  };
+
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="flex flex-row justify-between space-y-0">
         <CardTitle>Ingredients</CardTitle>
+        {/* Counter for settings desired servings */}
+        <div className="flex items-center flex-col sm:flex-row sm:gap-2">
+          <span className="text-lg font-semibold tracking-tight">
+            Porcions:
+          </span>
+          <div className="flex items-center">
+            <Button
+              className="rounded-none rounded-l-lg h-8 w-7 p-0"
+              onClick={decrement}
+            >
+              <Minus size={20} strokeWidth={3} />
+            </Button>
+            <Input
+              type="text"
+              value={servings}
+              className="mx-2 w-8 text-center p-0 h-8 rounded-none m-0 text-lg md:text-lg"
+              onChange={handleServingsChange}
+              onBlur={handleBlur}
+            />
+            <Button
+              className="rounded-none rounded-r-lg h-8 w-7 p-0"
+              onClick={increment}
+            >
+              <Plus size={20} strokeWidth={3} />
+            </Button>
+          </div>
+        </div>
       </CardHeader>
       <CardContent>
         <ul className="list-none">
