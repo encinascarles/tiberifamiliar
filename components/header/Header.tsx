e8d9d06adee1e01694e407384a085fe13f248ae1
Carles Encinas <carles@ganxo.net>
2024-05-04 20:27:10 +0200

    Stiled header searchBar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -12,18 +12,18 @@ import InvitationsModal from "./InvitationsDialog/InvitationsModal";
 
 const LoggedInHeader = () => (
   <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-    <div className="px-4 headerbp:px-8 container flex h-14 max-w-screen-2xl items-center">
-      <div className="mr-4 hidden headerbp:flex">
+    <div className="pr-4 pl-8 headerbp:pr-8 container flex h-14 max-w-screen-2xl items-center">
+      <div className="mr-4 hidden md:flex">
         <Logo />
         <TopNavigation />
       </div>
-      <div className="headerbp:hidden pr-4 items-center flex">
+      <div className="md:hidden pr-4 items-center flex">
         <MobileDrawerMenu />
       </div>
       <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
         <SearchBox />
         <NewRecipeButton />
-        <div className="hidden headerbp:block">
+        <div className="hidden md:block">
           <UserDropdownMenu />
         </div>
       </div>
@@ -34,8 +34,8 @@ const LoggedInHeader = () => (
 
 const LoggedOutHeader = () => (
   <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-    <div className="px-4 headerbp:px-8 container flex h-14 max-w-screen-2xl items-center">
-      <div className="mr-4 hidden headerbp:flex">
+    <div className="px-4 md:px-8 container flex h-14 max-w-screen-2xl items-center">
+      <div className="mr-4 hidden md:flex">
         <Logo />
       </div>
       <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
--- components/header/SearchBox.tsx
+++ components/header/SearchBox.tsx
@@ -1,12 +1,19 @@
 import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { Search } from "lucide-react";
 
 const SearchBox = () => {
   return (
-    <Input
-      type="text"
-      placeholder="Cerca receptes"
-      className="lg:w-60 headerbp:w-44 h-9"
-    />
+    <div className="flex w-full md:w-fit">
+      <Input
+        type="text"
+        placeholder="Cerca receptes"
+        className="w-full md:w-[200px] h-9 rounded-r-none"
+      />
+      <Button className="w-[35px] p-0 h-9 rounded-l-none">
+        <Search size={20} />
+      </Button>
+    </div>
   );
 };
 
